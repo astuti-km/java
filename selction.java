95b51e93692db02c94eabbf4fa05c160a750a250
Astuti Kumari <kumariastuti258@gmail.com>
2025-08-13 22:09:23 +0530

    Create selction.java

---
+++ selction.java
@@ -0,0 +1,30 @@
+//Selction sort
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        // Selection Sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j; // Update minIndex if smaller element found
+                }
+            }
+            // Swap the found minimum with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        // Print sorted array
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+output
+
+  Sorted array:
+11 12 22 25 64 
