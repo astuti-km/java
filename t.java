07a42429af86bafc2ea4a8a76667a1411b7ef909
Astuti Kumari <kumariastuti258@gmail.com>
2025-08-15 16:47:55 +0530

    Create t.java

---
+++ t.java
@@ -0,0 +1,85 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(+n+" * "+i+"= "+(n*i));
+        }    }
+}
+output
+19
+19 * 1= 19
+19 * 2= 38
+19 * 3= 57
+19 * 4= 76
+19 * 5= 95
+19 * 6= 114
+19 * 7= 133
+19 * 8= 152
+19 * 9= 171
+19 * 10= 190
+
+  import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int r=in.nextInt();
+       float area =3.14F*r*r;
+       System.out.println("The area of circle is: "+area);
+          }
+}
+output
+  7
+The area of circle is: 153.86002
+
+public class Main{
+    public static void main(String args[]){
+        String name="Astuti kumari";
+        String course ="BTECH";
+        String stream="CST";
+        String college= "UEM";
+        long enroll = 12024002022061L;
+        long regis = 304202400900621L;
+        int year = 2;
+        String sec="2A";
+        int roll = 15;
+        
+        System.out.println("Name: "+name +"\nCourse: "+course+"\nStream: "+stream+"\nCollege: "+college+"\nEnrollment number: "+enroll+"\n Registrattion numer: "+regis+"\nYear:"+year+"\nSection: "+sec+"\nClass Roll no.: "+roll);
+    }
+}
+output
+  Name: Astuti kumari
+Course: BTECH
+Stream: CST
+College: UEM
+Enrollment number: 12024002022061
+ Registrattion numer: 304202400900621
+Year:2
+Section: 2A
+Class Roll no.: 15
+
+  import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println("The even numbers  upto "+n+" are: ");
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+    System.out.print(i+" ");}}
+         
+    }
+}output
+  45
+The even numbers  upto 45 are: 
+2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 
+
+  
+
+
+
+
+  
+
+  
