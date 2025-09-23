77ee50712079bc47e8e5924ae60bcb817918a902
Astuti Kumari <kumariastuti258@gmail.com>
2025-09-23 19:54:48 +0530

    Create funcn.java

---
+++ funcn.java
@@ -0,0 +1,27 @@
+public class FunctionExample {
+    //funcn.using add two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = addNumbers(5, 7); // Calling the function
+        System.out.println("Sum is: " + result);
+    }
+}
+output
+Sum is: 12
+
+public class Basics {
+    // method that returns the square of a no.
+    public static int square(int num) {
+        return num * num;
+    }
+    public static void main(String[] args) {
+        int result = square(5);
+        System.out.println("Square is: " + result);
+    }
+}
+output
+Square is: 25
+
