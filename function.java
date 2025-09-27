00e4567d7ed1322cae48e3f3b31dd1e6bb7dbdd6
Astuti Kumari <kumariastuti258@gmail.com>
2025-09-27 21:15:16 +0530

    Create function.java

---
+++ function.java
@@ -0,0 +1,34 @@
+public class MyClass {
+
+public static void myMethod(int parameter1,String parameter2) {
+
+System.out.println("Parameter 1: " +
+
+
+parameter1);
+
+System.out.println("Parameter 2: " +
+
+parameter2);
+}
+
+public int addNumbers(int numi, int num2) {
+
+int sum num1, num2;
+
+return sum;
+
+}
+
+public static void main(String[] args) {
+
+myMethod(10, "Hello");
+
+MyClass obj = new MyClass();
+
+int result = obj.addNumbers(5, 7);
+
+System.out.println("Sum: " + result);
+
+
+}
