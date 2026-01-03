187ad6d775a1d5c03757f9a70ecbbb04580f2ece
Astuti Kumari <kumariastuti258@gmail.com>
2026-01-03 20:48:23 +0530

    Update 31.java

--- 31.java
+++ 31.java
@@ -33,3 +33,37 @@ class Main {
         System.out.println(newStr);
     }
 }
+
+class Solution {
+    public String addBinary(String s1, String s2) {
+        // code here
+        int i=s1.length()-1;
+        int j=s2.length()-1;
+        int carry=0;
+        String result="";
+        while(i>=0 ||j>=0){
+            int sum=carry;
+            if(i>=0)
+                sum+=s1.charAt(i--)-'0';
+            if(j>=0)
+                sum+=s2.charAt(j--)-'0';
+            
+            result=(sum%2)+result;
+            carry=sum/2;
+        }
+        if(carry==1)
+        result="1"+result;
+        
+        int index=0;
+        while(index<result.length()-1 && result.charAt(index)=='0'){
+            index++;
+        }
+      return result.substring(index);
+    }
+    public static void main(String args[]){
+        String s1="1011";
+        String s2="11";
+        Solution obj=new Solution();
+        obj.addBinary(s1,s2);
+    }
+}
